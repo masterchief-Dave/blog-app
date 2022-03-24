005f7cb2f3b54a7b9b5e5be73a1bc41af5c48b6c
Bodunrin David <bodunrindavidbond@gmail.com>
2022-03-24 13:20:31 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config({
+  path: './backend/.env'
+})
+const mongoose = require('mongoose')
+const app = require('./app')
+
+const uri = process.env.URI
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('connected to the database successfully')
+  })
+  .catch((err) => {
+    console.log(err)
+    console.log('error connecting to the database')
+  })
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+  console.log(`application started on ${PORT}`)
+})
