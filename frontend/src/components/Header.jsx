3f3c6d527227ca22f87415e1a5d0f2c59bf46b00
Bodunrin David <bodunrindavidbond@gmail.com>
2022-03-30 23:24:22 +0100

    Added the articles section and the categries section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { FaBars, FaTimes, FaCanadianMapleLeaf } from 'react-icons/fa'
+import { FaTimes, FaCanadianMapleLeaf } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import styles from './header.module.css'
+import { VscThreeBars } from 'react-icons/vsc'
 
 function Header() {
   const [click, setClick] = useState(false)
@@ -17,13 +18,20 @@ function Header() {
         <FaCanadianMapleLeaf id={styles.logo} size={33} />
 
         {click ? (
-          <FaTimes className='' size={23} onClick={handleClick} id={styles.times} />
+          <FaTimes
+            className={styles.icon}
+            size={33}
+            onClick={handleClick}
+            id={styles.times}
+          />
         ) : (
-          <FaBars className='' size={23} onClick={handleClick} id={styles.bar}/>
+          <VscThreeBars size={33} onClick={handleClick} id={styles.bar} />
         )}
-
       </div>
-      <nav id={click ? styles.navbar : styles.navbar_none} className={styles.navar}>
+      <nav
+        id={click ? styles.navbar : styles.navbar_none}
+        className={styles.navar}
+      >
         <ul id={styles.navbar_list}>
           <li>
             {' '}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,123 @@
 import React from 'react'
 import Header from '../components/Header'
 import styles from './home.module.css'
+import { FaRss } from 'react-icons/fa'
+import { MdPostAdd } from 'react-icons/md'
+import Bible from './../img/bible.jpg'
 
 function Home() {
   return (
-    <div id={styles.header}>
+    <div id={styles.home_section}>
       <Header />
-      this is the home page
+      <main>
+        <div id={styles.header_content}>
+          <h1 id={styles.header}> Blog </h1>
+          <div id={styles.commit}>
+            <a href="" id={styles.subscribe} className={styles.subscribe}>
+              <p href="" className={styles.subscribe_link}>
+                Subscribe to mailing list
+              </p>
+              <MdPostAdd size={20} />
+            </a>
+            <a href="" className={styles.subscribe}>
+              <p href="" className={styles.subscribe_link}>
+                Add to your Rss Feed
+              </p>
+              <FaRss size={15} />
+            </a>
+          </div>
+        </div>
+
+        <div id={styles.content}>
+          <div id={styles.articles}>
+            <article>
+              <div id={styles.content_img}>
+                <img src={Bible} alt="an image should be here " />
+              </div>
+
+              <div id={styles.content_text}>
+                <h2 id={styles.content_header}>
+                  Featured Book: Inclusion on Purpose
+                </h2>
+                <p id={styles.content_intro}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Animi, ratione accusantium! Consequuntur, deserunt reiciendis
+                  corporis molestias ab voluptatibus, quisquam magni accusamus
+                  nisi est sequi fugit, quibusdam delectus. Iure, corporis
+                  saepe?
+                </p>
+                <h5 id={styles.content_date}>Monday 5th september 2005</h5>
+                <hr />
+              </div>
+            </article>
+
+            {/* <article>
+              <div id={styles.content_img}>
+                <img src={Bible} alt="an image should be here " />
+              </div>
+
+              <div id={styles.content_text}>
+                <h2 id={styles.content_header}>
+                  Featured Book: Inclusion on Purpose
+                </h2>
+                <p id={styles.content_intro}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Animi, ratione accusantium! Consequuntur, deserunt reiciendis
+                  corporis molestias ab voluptatibus, quisquam magni accusamus
+                  nisi est sequi fugit, quibusdam delectus. Iure, corporis
+                  saepe?
+                </p>
+                <h5 id={styles.content_date}>Monday 5th september 2005</h5>
+                <hr />
+              </div>
+            </article> */}
+          </div>
+
+          <div id={styles.categories}>
+            <h3>categories</h3>
+            <div>
+              <ul className={styles.categories_list}>
+                <li>
+                  <a href="#">All</a>
+                </li>
+                <li>
+                  <a href="#">News</a>
+                </li>
+                <li>
+                  <a href="#">Life</a>
+                </li>
+                <li>
+                  <a href="#">Politics</a>
+                </li>
+                <li>
+                  <a href="#">Art</a>
+                </li>
+                <li>
+                  <a href="#">Food</a>
+                </li>
+                <li>
+                  <a href="#">Music</a>
+                </li>
+                <li>
+                  <a href="#">Gaming</a>
+                </li>
+                <li>
+                  <a href="#">Anime</a>
+                </li>
+                <li>
+                  <a href="#">Programming</a>
+                </li>
+                <li>
+                  <a href="#">Books</a>
+                </li>
+                <li>
+                  <a href="#">Humor</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
